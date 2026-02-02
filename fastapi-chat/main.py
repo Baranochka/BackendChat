5802f5eae9216cbc5de6705ccfd9c8dce93b30e7
Baranochka <Baranochka@users.noreply.github.com>
2026-02-02 22:03:43 +0500

    Create main.py

--- fastapi-chat/main.py
+++ fastapi-chat/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from backendchat!")
+import uvicorn
+from core import settings
+from fastapi import FastAPI
 
+app = FastAPI()
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        reload=True,
+    )
