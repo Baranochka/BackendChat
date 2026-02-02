0c119d00f62112a8f0de4e02688e4b56462978d3
Baranochka <Baranochka@users.noreply.github.com>
2026-02-02 23:22:08 +0500

    add lifespan

--- fastapi-chat/main.py
+++ fastapi-chat/main.py
@@ -2,8 +2,7 @@ import uvicorn
 from core import settings
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from core.models import db_helper
-from core.models import Base
+from core.models import db_helper, Base
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -15,7 +14,9 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 
-app = FastAPI()
+app = FastAPI( 
+    lifespan=lifespan,
+)
 
 if __name__ == "__main__":
     uvicorn.run(
