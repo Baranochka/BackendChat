f55da2027042a086543b9a50eaeb1f510e2fb6fe
Baranochka <Baranochka@users.noreply.github.com>
2026-02-02 22:04:07 +0500

    Create core with config.py

---
+++ fastapi-chat/core/config.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from pydantic import BaseModel, PostgresDsn
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).parent.parent
+
+
+class RunConfig(BaseModel):
+    host: str = "127.0.0.1"
+    port: str = 8000
+
+
+class ApiPrefix(BaseModel):
+    prefix: str = "/api"
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(
+            ".env.template",
+            ".env",
+        ),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP__",
+    )
+    run: RunConfig = RunConfig()
+    api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
+
+
+settings = Settings()
