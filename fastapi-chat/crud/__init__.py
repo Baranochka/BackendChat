71b62d6b4780367752fc56f413949924c591df79
Baranochka <Baranochka@users.noreply.github.com>
2026-02-03 01:57:43 +0500

    create crud get_chat in __init__

--- fastapi-chat/crud/__init__.py
+++ fastapi-chat/crud/__init__.py
@@ -1,2 +1,2 @@
-__all__=("create_chat", "create_message","get_list_messages")
-from .chat import create_chat, create_message, get_list_messages
+__all__=("create_chat", "create_message","get_list_messages", "get_chat")
+from .chat import create_chat, create_message, get_list_messages, get_chat
