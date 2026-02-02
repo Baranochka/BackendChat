fa17c089bef58e8fa996d270d6e06026201451c4
Baranochka <Baranochka@users.noreply.github.com>
2026-02-03 02:23:26 +0500

    change crud

--- fastapi-chat/core/schemas/chats.py
+++ fastapi-chat/core/schemas/chats.py
@@ -1,9 +1,19 @@
-from pydantic import BaseModel, ConfigDict
-
+from pydantic import BaseModel, ConfigDict, Field, StringConstraints
+from typing import Annotated
 class Chat(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    title: str
+    title: Annotated[
+        str,
+        StringConstraints(
+            strip_whitespace=True,
+            min_length=1,
+            max_length=200
+        )
+    ]
     
 class Message(BaseModel):
     model_config = ConfigDict(from_attributes=True)
-    text: str
+    text: str = Field(
+        min_length=1,
+        max_length=50020,
+    )
