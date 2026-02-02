b29b52de1246b22e1f097fa8a3c0aa01cdf87a68
Baranochka <Baranochka@users.noreply.github.com>
2026-02-02 23:46:49 +0500

    add args in func

--- fastapi-chat/api/chats.py
+++ fastapi-chat/api/chats.py
@@ -1,7 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from core import settings
+from core.schemas import Chat
+from core.models import db_helper
 router = APIRouter(prefix=settings.api.chats)
 
 @router.post("/")
-async def create_chats():
+async def create_chats(
+    chat: Chat,
+    session: AsyncSession = Depends(db_helper.session_getter)
+):
     pass
