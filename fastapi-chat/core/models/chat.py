80dd01d709a1f154ef5c3c8ca0fd696e4c3d990d
Baranochka <Baranochka@users.noreply.github.com>
2026-02-03 00:24:40 +0500

    change type for created_at

--- fastapi-chat/core/models/chat.py
+++ fastapi-chat/core/models/chat.py
@@ -1,16 +1,17 @@
 from .base import Base
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.types import TIMESTAMP
 from datetime import datetime
 from typing import List
 
 class Chat(Base):
     title: Mapped[str]
-    created_at: Mapped[datetime]
+    created_at: Mapped[datetime] = mapped_column(type_=TIMESTAMP(timezone=True))
     messages: Mapped[List["Message"]] = relationship(back_populates="chat")
     
 class Message(Base):
     chat_id: Mapped[int] = mapped_column(ForeignKey("chats.id"))
     chat: Mapped["Chat"] = relationship(back_populates="messages")
     text: Mapped[str]
-    created_at: Mapped[datetime]
+    created_at: Mapped[datetime] = mapped_column(type_=TIMESTAMP(timezone=True))
