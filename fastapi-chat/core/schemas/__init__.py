f42b5935c61fe5c08949626536a6ff2f09bec3c2
Baranochka <Baranochka@users.noreply.github.com>
2026-02-03 00:31:01 +0500

    schemas for Message

--- fastapi-chat/core/schemas/__init__.py
+++ fastapi-chat/core/schemas/__init__.py
@@ -1,2 +1,2 @@
-__all__=("Chat",)
-from .chats import Chat
+__all__=("Chat", "Message")
+from .chats import Chat, Message
--- fastapi-chat/core/schemas/chats.py
+++ fastapi-chat/core/schemas/chats.py
@@ -3,3 +3,7 @@ from pydantic import BaseModel, ConfigDict
 class Chat(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     title: str
+    
+class Message(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    text: str
